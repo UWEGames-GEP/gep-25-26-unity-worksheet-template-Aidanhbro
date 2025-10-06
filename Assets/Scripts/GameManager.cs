88daad2319f976f7dd3c2ccd4665836331014487
Aidan <aidan2.brown@live.uwe.ac.uk>
2025-10-06 12:21:04 +0100

    GameStatePause

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,7 +6,8 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public enum GameState { GAMEPLAY, ESCAPE, PAUSE, NULL };
     public GameState state = GameState.PAUSE;
-
+    bool hasChangedState = false;
+ 
     void Start()
     {
         state = GameState.PAUSE;
@@ -21,20 +23,43 @@ public class GameManager : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 state = GameState.PAUSE;
-
+                hasChangedState = true;
             }
+            
         }
         else if (state == GameState.PAUSE)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 state = GameState.GAMEPLAY;
-            
+                hasChangedState = true;
             }
+           
         }
 
 
        
+    }
+
+    private void LateUpdate()
+    {
+        if (hasChangedState)
+        {
+            hasChangedState = false;
+
+            if (state == GameState.GAMEPLAY) 
+            {
+                Time.timeScale = 1.0f;
+            }
+            else if (state == GameState.PAUSE) 
+            {
+                Time.timeScale = 0.0f;
+            }
+
+        }
+
+
+
     }
 
 
